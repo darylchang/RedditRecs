15fed63550a83b99647528cab4721ae15ea5fb34
Daryl Chang <darylc123@gmail.com>
2014-11-28 17:44:21 -0800

    Added item-based similarity

---
+++ item.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+from scipy.sparse import csr_matrix
+import networkx, pickle
+
+def dot(subA, subB):
+	result = 0.
+	for user in subA:
+		if user in subB:
+			result += subA[user] * subB[user]
+	return result
+
+def norm(sub):
+	return sum([sub[x]**2 for x in sub])**0.5
+
+partition = pickle.load(open('final_partition_subgraphs.pickle'))
+featureVectors = defaultdict(dict)
+similarityScores = defaultdict(dict)
+
+for line in open('publicvotes-20101018_votes.dump'):
+	user, link, sub, vote = line.split()
+	if user not in featureVectors[sub]:
+		featureVectors[sub][user] = int(vote)
+	else:
+		featureVectors[sub][user] += int(vote)
+
+for clusterID in range(len(set(partition.values()))):
+	subs = networkx.nodes(partition[clusterID])
+	for subA in subs:
+		for subB in subs:
+			if subA < subB:
+				featuresA, featuresB = featureVectors[A], featureVectors[B]
+				score = dot(featuresA, featuresB) / (norm(featuresA) * norm(featuresB))
+				print score
+				if subA not in similarityScores[clusterID]:
+					similarityScores[clusterID][subA] = {subB: score}
+				else:
+					similarityScores[clusterID][subA][subB] = score
+
+pickle.dump(similarityScores, open('similarity_scores.dump', 'w'))
+
