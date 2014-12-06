fe204f074bec1acdb48fe9692beb81bee144c8dd
Daryl Lei Chang <dlchang@corn19.stanford.edu>
2014-12-05 16:59:37 -0800

    Varied numUsers

--- cross_validation.py
+++ cross_validation.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from matplotlib import pyplot
 from scipy.sparse import csr_matrix
 import random
 import networkx, pickle
@@ -28,8 +29,8 @@ def getSimilarUsers(primary, sub, N):
             break
     return topNScores
 
-def testUser(user, sub):
-    similarUsers = getSimilarUsers(user, sub, 100)
+def testUser(user, sub, numUsers):
+    similarUsers = getSimilarUsers(user, sub, numUsers)
     cluster = 0
     for clusterID in range(len(partition)):
         subs = networkx.nodes(partition[clusterID])
@@ -44,18 +45,26 @@ def testUser(user, sub):
     sortedCommonSubs = sorted(commonSubs.items(), key = lambda x: x[1], reverse=True)
     topNRecs = []
     for x in sortedCommonSubs:
-        if x[1] > 10:
+        if x[1] > 0.1 * numUsers:
             topNRecs.append(x[0])
+    print len(topNRecs)
     return topNRecs
 
-successes = 0
-total = 0
-for i in range(0, 100):
-    user = powerUsers[random.randint(0, 890)]
-    sub = random.choice(featureVectors[user].keys())
-    recommendedSubs = testUser(user, sub)
-    for r in recommendedSubs:
-        total += 1
-        if r in featureVectors[user]:
-            successes += 1
-print successes * 1.0 / total
+numUserRange = [100, 500, 1000]
+precision = []
+
+for numUsers in numUserRange:
+    successes = 0
+    total = 0
+    for i in range(0, 100):
+        user = powerUsers[random.randint(0, 890)]
+        sub = random.choice(featureVectors[user].keys())
+        recommendedSubs = testUser(user, sub, 100)
+        for r in recommendedSubs:
+            total += 1
+            if r in featureVectors[user]:
+                successes += 1
+    precision.append(successes * 1.0 / total)
+
+pyplot.plot(numUserRange, precision)
+pyplot.show()
